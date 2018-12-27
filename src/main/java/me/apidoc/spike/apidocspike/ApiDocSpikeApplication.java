1af0413a9c241bcb1d7492343061ce5744a8e7df
Benwei Zhu <benweizhu@gmail.com>
2018-12-27 11:47:02 +0800

    init web

---
+++ src/main/java/me/apidoc/spike/apidocspike/ApiDocSpikeApplication.java
@@ -0,0 +1,14 @@
+package me.apidoc.spike.apidocspike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDocSpikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDocSpikeApplication.class, args);
+	}
+
+}
+
